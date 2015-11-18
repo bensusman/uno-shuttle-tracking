f949f1e92206909ebfccbe448e53bb433a601345
ctfu <cfu@my.bellevue.edu>
2015-11-18 00:13:32 -0600

    implement main board and lane board

--- ParkinglotDemo/_js/app.js
+++ ParkinglotDemo/_js/app.js
@@ -2,6 +2,13 @@ var myApp = angular.module('parkingDemo', []);
 
 myApp.controller('ParkinglotCtrl', function($scope){
 	$scope.lots = lotInfo;
+	$scope.lotId = 0;
+	$scope.selectedLot = 0;
+
+	$scope.setLot = function(newValue){
+		$scope.lotId = newValue;
+		$scope.selectedLot = $scope.lots[$scope.lotId];
+	};
 
 });
 
@@ -9,7 +16,7 @@ var lotInfo = [
 	{
 		"id" : 0,
 		"name" : "lot 1",
-		"totalSpace" : 20,
+		"totalSpace" : 30,
 		"lane" : [
 			{
 				"id" : 0,
@@ -24,6 +31,13 @@ var lotInfo = [
 				"totalSpace" : 10,
 				"status" : "available",
 				"availability" : 5
+			},
+			{
+				"id" : 3,
+				"name" : "lane 3",
+				"totalSpace" : 10,
+				"status" : "available",
+				"availability" : 3
 			}
 		]
 	},
